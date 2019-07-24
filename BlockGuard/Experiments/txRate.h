3d8c4c59fce085aa38d4d1db21f1b9f0c6b8633d
Kendric Hood <khood5@kent.edu>
2019-07-24 05:26:19 -0400

    fix includes

--- BlockGuard/Experiments/txRate.h
+++ BlockGuard/Experiments/txRate.h
@@ -6,11 +6,11 @@
 #define DISTRIBUTED_CONSENSUS_ABSTRACT_SIMULATOR_TXRATE_H
 
 #include <iostream>
-#include "params.hpp"
+#include "../params.hpp"
 #include "metrics.hpp"
-#include "bCoinReferenceCommittee.hpp"
-#include "PBFTReferenceCommittee.hpp"
-#include "SBFTReferenceCommittee.hpp"
+#include "../bCoin/bCoinReferenceCommittee.hpp"
+#include "../PBFT/PBFTReferenceCommittee.hpp"
+#include "../SBFT/SBFTReferenceCommittee.hpp"
 #include "Sharded_PBFT_Experiments.hpp"
 #include <fstream>
 
